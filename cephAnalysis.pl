f08add88a6e42e7213592eaa90ea475c52f717ac
Dave Sims <misc@lamneth.net>
2014-03-27 17:56:20 -0400

    Updated for TVCv4.0; Beta version

--- cephAnalysis.pl
+++ cephAnalysis.pl
@@ -9,11 +9,16 @@
 
 use warnings;
 use strict;
-use List::Util qw{ min max sum };
-use Data::Dumper;
+use autodie;
 
-( my $scriptname = $0 ) =~ s/^(.*\/)+//;
-my $version = "v3.0.0";
+use List::Util qw{ min max sum };
+use Getopt::Long qw{ :config no_ignore_case };
+use File::Basename;
+use Data::Dump;
+
+#( my $scriptname = $0 ) =~ s/^(.*\/)+//;
+my $scriptname = basename($0);
+my $version = "v4.0.032714";
 my $description = <<EOT;
 Program to read in all of the variant call table files from an Ion Torrent run on CEPH, and report out
 the ID and number of times each variant is seen.  This is used to track the number of variants reported 
@@ -23,7 +28,7 @@ EOT
 
 my $usage = <<EOT;
 USAGE: $scriptname [options] <input_files>
-    -d, --dev        Development Version that works with format of TVCv4.0
+    -C, --Classic    Use TVCv3.2.1 formatted data files.  Will be deprecated.
     -c, --coverage   Coverage cutoff (default is 450)
     -p, --preview    Write output only to STDOUT to preview the results.
     -o, --output     Write output to file (default is STDOUT)
@@ -31,54 +36,43 @@ USAGE: $scriptname [options] <input_files>
     -h, --help       Print help information
 EOT
 
-sub help {
-	printf "%s - %s\n%s\n\n%s\n", $scriptname, $version, $description, $usage;
-	exit;
-}
-
 my $verinfo;
 my $help;
 my $covfilter = 450;
 my $preview;
 my $output;
-my $dev_version;
+my $classic;
 
 # Set up some commandline opts
-while ( scalar( @ARGV ) > 0 ) {
-	last if ( $ARGV[0] !~ /^-/ );
-	my $opt = shift;
-    if    ( $opt eq '-d' || $opt eq '--dev' )      { $dev_version = 1; }
-	elsif ( $opt eq '-o' || $opt eq '--output' )   { $output = shift; }
-	elsif ( $opt eq '-p' || $opt eq '--preview' )  { $preview = 1; }
-	elsif ( $opt eq '-c' || $opt eq '--coverage' ) { $covfilter = shift; }
-	elsif ( $opt eq '-v' || $opt eq '--version' )  { $verinfo = 1; }
-	elsif ( $opt eq '-h' || $opt eq '--help' )     { $help = 1; }
-	else {
-		print "$scriptname: Invalid Option '$opt'\n";
-		print $usage;
-		exit 1;
-	}
-}
+GetOptions( "Classic"       => \$classic,
+            "coverage=i"    => \$covfilter,
+            "preview"       => \$preview,
+            "output=s"      => \$output,
+            "version"       => \$verinfo,
+            "help"          => \$help,
+    ) or die "\n$usage";
 
-if ( $verinfo ) {
-	print "$scriptname\t$version\n";
+sub help {
+	printf "%s - %s\n%s\n\n%s\n", $scriptname, $version, $description, $usage;
 	exit;
 }
 
-help if $help;
-
-if ( $covfilter !~ /\d+/ ) {
-	print "ERROR: coverage cutoff must be numeric!\n";
-	exit 1;
+sub version {
+    printf "%s - %s\n", $scriptname, $version;
+    exit;
 }
 
-my @filesList;
-die "No files loaded for analysis!\n" if ( ! defined @ARGV );
+version if $verinfo;
+help if $help;
 
+my @filesList;
 my $totRuns;
-foreach my $file ( @ARGV ) {
-	push( @filesList, $file);
-	$totRuns = scalar( @filesList );
+if ( ! @ARGV ) {
+    print "ERROR: no files loaded for analysis\n";
+    exit 1;
+} else {
+    @filesList = @ARGV;
+    $totRuns = scalar @filesList;
 }
 
 # Set up Tally output file.
@@ -92,58 +86,66 @@ if ( $preview ) {
 }
 
 ######--------------------------------- END COMMANDLINE ARGS ----------------------------------------######
-my ( %varCount, %trimHash );
-my ( %varFreq, %varCov );
+my ( %all_variants, %var_freq, %var_cov );
 
-# Get the variant call data from each input file and start collected in the data in a hash
-foreach my $varFile ( @filesList ) {
-	( my $filename = $varFile ) =~ s/\.txt//;
+# Set up fields to use to capture data from the tables.  Set up here so that we can mod it easy later when the 
+# tables change.
+my %fields = (
+    "v3.2.1"  => { "varid" => [qw(2 0 1 6 7)],
+                   "data"  => [qw(0 1 2 3 6 7 8 10)],
+                 },
+    "v4.0.2"  => { "varid" => [qw(12 0 1 15 16)],
+                   "data"  => [qw(0 1 12 13 15 16 6 18)],
+                 }
+);
 
-	open( VARFILE, "<", $varFile ) || die "Error opening file: $varFile. $!";
-	
-	while (<VARFILE>) {
-        next if ( /Chrom/ || /Absent/ || /No Call/ );
-        my @line = split( /\t/, $_ );
-        if ( ! defined $dev_version ) {
-            #next if (/Chrom/); 
-            #my @line = split;
-            #my @line = split( /\t/, $_ );
-            
-            if ( $line[10] > $covfilter ) {
-                my $varID = join( ":", @line[2,0,1,6,7] ); #Gene:Chr:Pos:REF:ALT
-                $varCount{$varID}++;
-                push( @{$trimHash{$varID}}, @line[0,1,2,3,6,7,8,10] );
-                push( @{$varFreq{$varID}}, $line[8] );
-                push( @{$varCov{$varID}}, $line[10] );
-            }
-        } else {
-            # Set up correct fields for TVCv4.0
-            if ( $line[18] > $covfilter ) {
-                my $varID = join( ":", @line[12,0,14,15,16] );
-                $varCount{$varID}++;
-                push( @{$trimHash{$varID}}, @line[0,14,12,13,15,16,6,18] );
-                push( @{$varFreq{$varID}}, $line[6] );
-                push( @{$varCov{$varID}}, $line[18] );
-                #print "$varID\n";
-                #next;
+proc_datatable( \@filesList, \%fields );
+
+sub proc_datatable {
+    my $files = shift;
+    my $data_fields = shift;
+
+    # Get fields to use for spliting the tables
+    my $version = $classic ? 'v3.2.1' : 'v4.0.2';
+    print "Processing '$version' data...\n";
+    my @varid_index = @{$$data_fields{$version}{'varid'}};
+    my @field_index = @{$$data_fields{$version}{'data'}};
+
+    for my $file ( @$files ) {
+        open( my $in_fh, "<", $file );
+        while (<$in_fh>) {
+            next if ( /Chrom/ || /Absent/ || /No Call/ );
+            my @fields = split;
+            if ( $fields[$field_index[7]] > $covfilter ) {
+                my $varid = join( ':', @fields[@varid_index] );
+                push( @{$all_variants{$varid}}, [@fields[@field_index]] );
+                #push( @{$all_variants{$varid}->{@fields[@field_index[0..5]]}}, [@fields[@field_index[6,7]]] );
+                #push( @{$all_variants{$varid}}, [$fields[$field_index[6]], $fields[$field_index[7]]] ); 
+                push( @{$var_freq{$varid}}, $fields[$field_index[6]] );
+                push( @{$var_cov{$varid}}, $fields[$field_index[7]] );
             }
         }
-	}
+        close $in_fh;
+    }
+    return;
 }
-close( VARFILE );
 
 # Get some field width data
-my ( $rwidth, $awidth ) = field_width( \%trimHash );
+my ( $rwidth, $awidth ) = field_width( \%all_variants );
+#print "rwidth: $rwidth\nawidth: $awidth\n";
+
+#dd \%all_variants;
+#exit;
 
 # Get statistics about each variant and generate a formated hash table to print out the results with 
 my %results;
-foreach my $variant ( sort keys %varCount ) {
-	my $count = $varCount{$variant};
+foreach my $variant ( keys %all_variants) {
+	my $count = @{$all_variants{$variant}};
     my ( $gene, $chr, $pos, $ref, $alt ) = split( /:/, $variant );
 	
 	# Get min, max, median coverage and frequency info	
-	my ( $minCov, $maxCov, $meanCov ) = stats( \@{$varCov{$variant}} );
-	my ( $minFreq, $maxFreq, $meanFreq ) = stats( \@{$varFreq{$variant}} );
+	my ( $minCov, $maxCov, $meanCov ) = stats( \@{$var_cov{$variant}} );
+	my ( $minFreq, $maxFreq, $meanFreq ) = stats( \@{$var_freq{$variant}} );
     my $detection_freq = sprintf( "%0.2f", ($count/$totRuns)*100);
 
     my $format = "%-8s %-8s %-12d %-${rwidth}s %-${awidth}s %-10s %-7d %-7d %-7d %8.2f%% %8.2f%% %8.2f%%";
@@ -182,7 +184,8 @@ sub field_width {
     my $awidth = 0;
 
     for my $var ( keys %$hash_ref ) {
-        my ($ref,$alt) = $var =~ /.*:(\w+):(\w+)$/;
+        my ($ref, $alt) = $var =~ /.*?(\w+):(\w+)$/;
+        #print "ref: $ref\nalt: $alt\n";
         
         $rwidth = length( $ref ) + 3  if ( length( $ref ) > $rwidth ); 
         $awidth = length( $alt ) + 3  if ( length( $alt ) > $awidth );
