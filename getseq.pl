5c7182b4db2915b9aa480fd8c10c1585d1ab8c63
Dave Sims <misc@lamneth.net>
2015-01-26 15:27:06 -0500

    Added capability for batch analysis. Removed the need to pass coords as strings. Updated and modernized code.

--- getseq.pl
+++ getseq.pl
@@ -10,16 +10,19 @@
 #
 # 6/15/2013 - D Sims
 ##############################################################################################################
-
 use warnings;
 use strict;
-use Data::Dumper;
+use autodie;
+
+use Getopt::Long qw( :config bundling auto_abbrev no_ignore_case );
+use File::Basename;
+use Data::Dump;
 use LWP::Simple;
 use XML::Twig;
 use Sort::Versions;
 
-( my $scriptname = $0 ) =~ s/^(.*\/)+//;
-my $version = "v0.7.040214"; # add version sort to output
+my $scriptname = basename($0);
+my $version = "v1.0.0_012615";
 my $description = <<"EOT";
 Program to retrieve sequence from the UCSC DAS server.  Enter sequence coordinates in the form of 'chr:start-stop',
 and the output will be sequence from hg19, padded by 10 bp.  Extra padding can be added with the '-p' option.  
@@ -35,44 +38,46 @@ The sequence can be fed from a file list or can be entered one by one on the CLI
 EOT
 
 my $usage = <<"EOT";
-USAGE: $0 [options] <chr:start-stop>
+USAGE: $scriptname [options] <chr:start-stop>
     -p, --pad       Pad the output sequence (Default is 10bp).
+    -b, --batch     Load up a batch file of positions to search.
     -o, --output    Send output to custom file.  Default is STDOUT.
     -v, --version   Version information
     -h, --help      Print this help information
 EOT
+
+my $help;
+my $ver_info;
+my $outfile;
+my $padding = 10;
+my $batch_file;
+
+GetOptions( "padding|p=i"   => \$padding,
+            "batch|b=s"     => \$batch_file,
+            "output|o=s"    => \$outfile,
+            "version|v"     => \$ver_info,
+            "help|h"        => \$help )
+        or die $usage;
 
 sub help {
 	printf "%s - %s\n\n%s\n\n%s\n", $scriptname, $version, $description, $usage;
 	exit;
 }
 
-sub version_info {
+sub version {
 	printf "%s - %s\n", $scriptname, $version;
 	exit;
 }
 
-my $help = 0;
-my $verInfo = 0;
-my $outfile;
-my $padding = 10;
-
-while ( scalar( @ARGV ) > 0 ) {
-	last if ( $ARGV[0] !~ /^-/ );
-	my $opt = shift;
-	if    ( $opt eq '-v' || $opt eq '--version' )  { $verInfo = 1; }
-    elsif ( $opt eq '-p' || $opt eq '--pad' )      { $padding = shift; }
-	elsif ( $opt eq '-h' || $opt eq '--help' )     { $help = 1; }
-	elsif ( $opt eq '-o' || $opt eq '--output' )   { $outfile = shift; }
-	else {
-		print "Invalid option: '$opt'\n";
-		print "$usage\n";
-		exit 1;
-	}
-}
-
 help if $help;
-version_info if $verInfo;
+version if $ver_info;
+
+# Make sure enough args passed to script
+if ( scalar( @ARGV ) < 1 && ! $batch_file ) {
+    print "ERROR: need to enter at least one coordinate to search!\n\n";
+    print "$usage\n";
+    exit 1;
+}
 
 # Write output to either indicated file or STDOUT
 my $out_fh;
@@ -83,26 +88,18 @@ if ( $outfile ) {
 }
 
 #########------------------------------ END ARG Parsing ---------------------------------#########
-
 my @queries;
-my %lookup;
 
-# Read in position file and create a query line. Position has to be in format chr*\tstart\tstop
-while (<>) {
-	chomp;
-	my $queryline;
-	
-	# Get query components to set up the query string.
-	my ( $chr, $start, $end )  =  $_ =~ /^(?:chr)?(\d+).(\d+)(?:\D(?:[\-,\. ])?(\d+))?$/;
-    $end = $start if ( ! $end );
-
-    # Add some padding to the start and end positions
-    $start -= $padding;
-    $end += $padding;
-
-	# Make sure that the sequence of start and end is right before loading.  Gets a silent error otherwise!
-	( $end > $start ) ? $queryline = "chr$chr:$end,$start" : $queryline = "chr$chr:$start,$end";
-	push( @queries, $queryline );
+if ($batch_file) {
+    my @batch_list = proc_batch( $batch_file );
+    for my $coord (@batch_list) {
+        my $formatted_query = gen_queries($coord);
+        push(@queries, $formatted_query);
+    }
+} else {
+    my $input_query = shift;
+    my $formatted_query = gen_queries($input_query);
+    push(@queries, $formatted_query);
 }
 
 my $URL = "http://genome.ucsc.edu/cgi-bin/das/hg19/dna?segment=";
@@ -121,13 +118,32 @@ foreach ( @queries ) {
 
 # Print out the formatted results
 for ( sort { versioncmp( $a, $b ) } keys %result ) {
-
     ( my $seq_id = $_) =~ s/,/-/g;
-    print $seq_id."\n".uc($result{$_})."\n";
+    print {$out_fh} ">$seq_id" . uc($result{$_}) . "\n";
+}
 
-    # TODO: Configure fancy MOI output
-    # Fancy formatting with a box around the MOI...needs work!
-	#( my $anchorStr = $result{$_} ) =~ s/(.{10})(.)(.{10})/$1 \[$2\] $3/;
-	#printf "%-30s%s\n", $_, $anchorStr;
-    
+sub gen_queries {
+    my $input_string = shift;
+
+    my ($chr, $start, $end) = $input_string =~ /^(?:chr)?(\d+).(\d+)(?:\D(?:[\-,\. ])?(\d+))?$/;
+    $end = $start unless $end;
+    $start -= $padding;
+    $end += $padding;
+
+    my $formatted_query;
+    # TODO:  Bug with the string if someone has put the end before the start.
+    ($end > $start) ? $formatted_query = "chr$chr:$end,$start" : $formatted_query = "chr$chr:$start,$end";
+
+    return $formatted_query;
+}
+
+sub proc_batch {
+    # read in a batch file if many positons needed
+    my $input_file = shift;
+
+    open(my $fh, "<", $input_file);
+    my @query_list = map { chomp($_); $_ } <$fh>;
+    close $fh;
+
+    return @query_list;
 }
